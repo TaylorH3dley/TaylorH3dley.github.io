d7dce3a2247b31420a0ad7b5c83070cb0232fc4c
TaylorH3dley <taylor.hedley2@gmail.com>
2025-04-28 09:15:50 -0700

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -89,18 +89,11 @@ const CONFIG = {
   ],
   experiences: [
     {
-      company: 'Company Name',
-      position: 'Position',
-      from: 'September 2021',
+      company: 'In-N-Out Burger',
+      position: 'Level 6 Associate',
+      from: 'April 2019',
       to: 'Present',
-      companyLink: 'https://example.com',
-    },
-    {
-      company: 'Company Name',
-      position: 'Position',
-      from: 'July 2019',
-      to: 'August 2021',
-      companyLink: 'https://example.com',
+      companyLink: '',
     },
   ],
   certifications: [
